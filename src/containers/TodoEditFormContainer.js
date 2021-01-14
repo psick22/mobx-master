44853fbd24c3af307290d6916a276de7d8112087
psick22 <kgirok@gmail.com>
2021-01-14 10:34:38 +0900

    Initialize mobx master project

---
+++ src/containers/TodoEditFormContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import TodoEditFormView from '../views/TodoEditFormView';
+import { inject, observer } from 'mobx-react';
+import autobind from 'autobind-decorator';
+// bind 뭔지 공부
+
+@inject('todoStore')
+@autobind
+@observer
+class TodoEditFormContainer extends Component {
+  onSetTodoProps(name, value) {
+    this.props.todoStore.setTodoProps(name, value);
+  }
+
+  render() {
+    const { todoStore } = this.props;
+
+    return (
+      <TodoEditFormView
+        todo={todoStore.todo}
+        onSetTodoProps={this.onSetTodoProps}
+      />
+    );
+  }
+}
+
+export default TodoEditFormContainer;
---
+++ src/stores/TodoStore.js
@@ -0,0 +1,24 @@
+import { observable, action, computed, makeObservable } from 'mobx';
+
+class TodoStore {
+  constructor() {
+    makeObservable(this);
+  }
+
+  @observable
+  _todo = {}; // id, title, date
+
+  get todo() {
+    return this._todo;
+  }
+
+  @action
+  setTodoProps(name, value) {
+    this._todo = {
+      ...this._todo,
+      [name]: value,
+    };
+  }
+}
+
+export default new TodoStore();
